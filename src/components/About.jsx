3fa023bdcf5dfb432211e53eaff02ea9fd344d8a
dakotafabro <dakotafabrocgu@gmail.com>
2022-08-01 10:53:00 -0700

    updated icon - test

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -282,7 +282,7 @@ function About() {
     },
     {
       column_size: "col-sm-2",
-      icon_class: "fa-solid fa-code-simple",
+      icon_class: "fas fa-code-simple",
       description: "PostgreSQL",
     },
   ];
