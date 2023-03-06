893f98f7ee7ba4dbaf105be6097e7a57773f42ff
dakotafabro <dakotafabrocgu@gmail.com>
2023-03-06 10:19:48 -0800

    added new projects

--- src/staticInfo/workExperience.js
+++ src/staticInfo/workExperience.js
@@ -1,7 +1,28 @@
 export const workExperience = [
   {
+    id: "workitem11",
+    title: "Choose Connection Toolkit",
+    description:
+      "Modeled after Nonviolent Communication practices, my hope is that this toolkit can help professionals write effective emails, partners to communicate clearly and thoughtfully, and/or friends to build an effective response should they find themselves in conflict. Just like all of us, this toolkit is a work in progress 💛. Built with ReactJS, Bootstrap, and Dictionary API.",
+    image: "https://i.imgur.com/HF5iZ2D.png",
+    buttonText: "Build a response",
+    buttonLink: "https://chooseconnection.netlify.app/",
+    year: 2023,
+  },
+  {
+    id: "workitem10",
+    title: "MentyB API",
+    description:
+      "A mental-health centered API that allows users to query an emotion to receive a list of tasks or actions that can be taken as it pertains to that emotion and each sphere of wellness. Built with Node.js, Express, and MongoDB.",
+    image: "https://i.imgur.com/HPOq8zP.png",
+    buttonText: "API Docs",
+    buttonLink:
+      "https://gist.github.com/dakotafabro/44305fd0ac7bd451b2c3252dd5abaae2",
+    year: 2023,
+  },
+  {
     id: "workitem9",
-    title: "HangOut - Social App - Fullstack Mobile App",
+    title: "HangOut",
     description:
       "A social app connecting users through community events and activities. Project was built using React Native, Redux, Firebase, JavaScript, HTML, CSS, and Tailwind. Website built during Fullstack Bootcamp by Donna Sayos, Amy Ji, and Me.",
     image: "https://i.imgur.com/tIRFq4u.png",
@@ -11,8 +32,7 @@ export const workExperience = [
   },
   {
     id: "workitem8",
-    title:
-      "The Margaret Hamilton Academy of JavaScript - School CRM - Fullstack Web App",
+    title: "The Margaret Hamilton Academy of JavaScript",
     description:
       "A school CRM that tracks student information and their associated campuses. Project was built using ReactJS, Redux, PostrgreSQL, Sequelize, NodeJS, ExpressJS, JavaScript, HTML, CSS, and Bootstrap. Website built during Fullstack Bootcamp.",
     image: "https://i.imgur.com/ggaxckq.png",
@@ -32,7 +52,7 @@ export const workExperience = [
   },
   {
     id: "workitem6",
-    title: "Self-Care Assistant - Fullstack Web App",
+    title: "Self-Care Assistant",
     description:
       "Sometimes thinking of something to do for self-care or getting oneself out of a rut can be hard. Self-Care Assistant is here to help! This project features self-care, personal, and work suggestions when it's too hard to think of what to do on one's own with the click of a button. Project was built using JavaScript, HTML, CSS, and The Bored API. Website built for fun!",
     image: "https://i.imgur.com/r3Iq2Ma.png",
@@ -40,59 +60,59 @@ export const workExperience = [
     buttonLink: "https://self-care-assistant.netlify.app/",
     year: 2022,
   },
-  {
-    id: "workitem5",
-    title: "Guessing Game - Frontend - VanillaJS",
-    description:
-      "Can you guess the number? Project was built using JavaScript, HTML, CSS, and Bootstrap. Website built during Fullstack Bootcamp.",
-    image: "https://i.imgur.com/3M9rwrq.png",
-    buttonText: "Take a Guess",
-    buttonLink: "https://df-guessinggame.netlify.app/",
-    year: 2022,
-  },
-  {
-    id: "workitem4",
-    title: "Dictionary Search Engine - ReactJS",
-    description:
-      "This dictionary utilizes Pexel's extraordinary photo API along with Free Dictionary's API to create a fully functional dictionary search engine. Built during my time in SheCodes.",
-    image: "https://i.imgur.com/XrPu5Dm.png",
-    buttonText: "See Dictionary App",
-    buttonLink: "https://dfabro-dictionary-react-app.netlify.app/",
-    year: 2021,
-  },
-  {
-    id: "workitem3",
-    title: "Note Taking Application - ReactJS",
-    description:
-      "In addition to Dark Mode toggling capabilities, this app utilizes local storage to create a fully functional note-taking app. Built out of curiosity",
-    image: "https://i.imgur.com/JKB9hRb.png",
-    buttonText: "Take Notes Here",
-    buttonLink: "https://note-taking-app-dakotafabro.netlify.app/",
-    year: 2021,
-  },
-  {
-    id: "workitem2",
-    title: "Weather Application - ReactJS",
-    description:
-      "This most recent project that pays homage to my first coding project below. Weather App built with ReactJS, Bootstrap, and API Integration. Built during my time in SheCodes.",
-    image: "https://i.imgur.com/ARy1kSR.png",
-    buttonText: "Check The Weather",
-    buttonLink: "https://dfabro-react-weather-app-new.netlify.app/",
-    year: 2021,
-  },
-  {
-    id: "workitem1",
-    title: "Weather Application - Vanilla JS",
-    description:
-      "This project was one of my earliest front-end projects. Project built with HTML, CSS, Vanilla JS, Bootstrap, and API Integration. Built during my time in SheCodes.",
-    image: "https://i.imgur.com/MzLMjDl.png",
-    buttonText: "Check The Weather",
-    buttonLink: "https://weather-app-dfabro-shecodes.netlify.app/",
-    year: 2021,
-  },
+  // {
+  //   id: "workitem5",
+  //   title: "Guessing Game - Frontend - VanillaJS",
+  //   description:
+  //     "Can you guess the number? Project was built using JavaScript, HTML, CSS, and Bootstrap. Website built during Fullstack Bootcamp.",
+  //   image: "https://i.imgur.com/3M9rwrq.png",
+  //   buttonText: "Take a Guess",
+  //   buttonLink: "https://df-guessinggame.netlify.app/",
+  //   year: 2022,
+  // },
+  // {
+  //   id: "workitem4",
+  //   title: "Dictionary Search Engine - ReactJS",
+  //   description:
+  //     "This dictionary utilizes Pexel's extraordinary photo API along with Free Dictionary's API to create a fully functional dictionary search engine. Built during my time in SheCodes.",
+  //   image: "https://i.imgur.com/XrPu5Dm.png",
+  //   buttonText: "See Dictionary App",
+  //   buttonLink: "https://dfabro-dictionary-react-app.netlify.app/",
+  //   year: 2021,
+  // },
+  // {
+  //   id: "workitem3",
+  //   title: "Note Taking Application - ReactJS",
+  //   description:
+  //     "In addition to Dark Mode toggling capabilities, this app utilizes local storage to create a fully functional note-taking app. Built out of curiosity",
+  //   image: "https://i.imgur.com/JKB9hRb.png",
+  //   buttonText: "Take Notes Here",
+  //   buttonLink: "https://note-taking-app-dakotafabro.netlify.app/",
+  //   year: 2021,
+  // },
+  // {
+  //   id: "workitem2",
+  //   title: "Weather Application - ReactJS",
+  //   description:
+  //     "This most recent project that pays homage to my first coding project below. Weather App built with ReactJS, Bootstrap, and API Integration. Built during my time in SheCodes.",
+  //   image: "https://i.imgur.com/ARy1kSR.png",
+  //   buttonText: "Check The Weather",
+  //   buttonLink: "https://dfabro-react-weather-app-new.netlify.app/",
+  //   year: 2021,
+  // },
+  // {
+  //   id: "workitem1",
+  //   title: "Weather Application - Vanilla JS",
+  //   description:
+  //     "This project was one of my earliest front-end projects. Project built with HTML, CSS, Vanilla JS, Bootstrap, and API Integration. Built during my time in SheCodes.",
+  //   image: "https://i.imgur.com/MzLMjDl.png",
+  //   buttonText: "Check The Weather",
+  //   buttonLink: "https://weather-app-dfabro-shecodes.netlify.app/",
+  //   year: 2021,
+  // },
   {
     id: "workitem0",
-    title: "Game Development",
+    title: "Hup Hup",
     description:
       "My first experience with coding was building games using C# and Unity. Hup-Hup was my first multi-level game that used complex C# scripts. Project was built using C#, Unity, and Visual Studio Code. Built for fun!",
     image: "https://i.imgur.com/fRkrlWn.png",
